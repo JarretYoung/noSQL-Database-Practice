0abd2b838f854a0ae247a262154df4143581e40d
Garret Yong <gyon0004@student.monash.edu>
2023-04-19 17:05:02 +0800

    Week 7 stuff

--- Applied/App07/week7_schema.sql
+++ Applied/App07/week7_schema.sql
@@ -1,3 +1,5 @@
+set echo on 
+spool week7_schema_output.txt
 /*
 Databases Week 7 Tutorial Class
 week7_schema.sql
@@ -13,9 +15,11 @@ Author:
 --
 -- Place DROP commands at head of schema file
 --
-
-
-
+drop table enrolment purge;
+drop table student purge;
+drop table course_unit purge;
+drop table unit purge;
+drop table course purge;
 -- Create Tables
 -- Here using both table and column constraints
 --
@@ -40,11 +44,11 @@ COMMENT ON COLUMN student.stu_dob IS
     'Student date of birth';
 
 /* Add STUDENT constraints here */
-
+ALTER TABLE student ADD CHECK( stu_nbr > 10000000);
 /* Add UNIT data types here */
 CREATE TABLE unit (
-    unit_code   ,
-    unit_name   
+    unit_code   CHAR(7),
+    unit_name   VARCHAR2(50)
 );
 
 COMMENT ON COLUMN unit.unit_code IS
@@ -54,15 +58,16 @@ COMMENT ON COLUMN unit.unit_name IS
     'Unit name';
 
 /* Add UNIT constraints here */
+ALTER TABLE unit add UNIQUE (unit_Name);
 
 /* Add ENROLMENT attributes and data types here */
 CREATE TABLE enrolment (
-    ,
-    ,
-    ,
-    ,
-    ,
-    
+    stu_nbr NUMBER(8),
+    unit_code CHAR(7),
+    enrol_year NUMBER(4),
+    enrol_semester CHAR(1),
+    enrol_mark NUMBER(3),
+    enrol_grade CHAR(2)
 );
 
 COMMENT ON COLUMN enrolment.stu_nbr IS
@@ -84,3 +89,8 @@ COMMENT ON COLUMN enrolment.enrol_grade IS
     'Enrolment grade (letter)';
 
 /* Add ENROLMENT constraints here */
+ALTER TABLE enrolment ADD CHECK(enrol_semester = 1 or enrol_semester = 2 or enrol_semester = 3);
+ALTER TABLE enrolment ADD CONSTRAINT enrolment_pk PRIMARY KEY ( stu_nbr, unit_code, enrol_year, enrol_semester );
+
+spool off 
+set echo off 
