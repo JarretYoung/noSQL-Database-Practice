c7ba4806b19f67f288a450a5e835bba33b3e21df
Garret Yong <gyon0004@student.monash.edu>
2023-04-29 14:22:38 +0800

    Initial push

---
+++ Assignments/Ass2A/sql_portfolio1/sql_portfolio1.sql
@@ -0,0 +1,37 @@
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SET ECHO ON
+SPOOL sql_portfolio1_output.txt
+
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--sql_portfolio1.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Tutorial Class No:
+
+
+/*Task 1: CREATE table POLICY and non FK constraints*/
+
+
+
+
+/*Task 1: Add FK constraints*/
+
+
+
+/*Task 2*/
+
+
+
+/*Task 3*/
+
+
+
+/*Task 4*/
+
+
+
+--Comment out SET ECHO and SPOOL commands before submitting your portfolio
+SPOOL OFF
+SET ECHO OFF
