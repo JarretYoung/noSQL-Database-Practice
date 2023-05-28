1c2f83f73f1ec7839e7ef85f0bd5baa06579403e
Garret Yong <gyon0004@student.monash.edu>
2023-05-28 23:04:19 +0800

    Initial push of T3

---
+++ Assignments/Ass2B/T3-tsa-json.sql
@@ -0,0 +1,21 @@
+--*****PLEASE ENTER YOUR DETAILS BELOW*****
+--T3-tsa-json.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Applied Class No:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+/*3(a)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT TO GENERATE 
+-- THE COLLECTION OF JSON DOCUMENTS HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
---
+++ Assignments/Ass2B/T3-tsa-mongo.mongodb.js
@@ -0,0 +1,38 @@
+// *****PLEASE ENTER YOUR DETAILS BELOW*****
+// T3-tsa-mongo.mongodb.js
+
+// Student ID:
+// Student Name:
+// Unit Code:
+// Applied Class No:
+
+//Comments for your marker:
+
+
+//Use (connect to) your database - you MUST update xyz001
+//with your authcate username
+use("xyz0001");
+
+//3(b)
+// PLEASE PLACE REQUIRED MONGODB COMMAND TO CREATE THE COLLECTION HERE
+// YOU MAY PICK ANY COLLECTION NAME
+// ENSURE that your query is formatted and has a semicolon
+// (;) at the end of this answer
+
+
+//3(c)
+// PLEASE PLACE REQUIRED MONGODB COMMAND/S FOR THIS PART HERE
+// ENSURE that your query is formatted and has a semicolon
+// (;) at the end of this answer
+
+
+///3(d)
+// PLEASE PLACE REQUIRED MONGODB COMMAND/S FOR THIS PART HERE
+// ENSURE that your query is formatted and has a semicolon
+// (;) at the end of this answer
+
+
+//3(e)
+// PLEASE PLACE REQUIRED MONGODB COMMAND/S FOR THIS PART HERE
+// ENSURE that your query is formatted and has a semicolon
+// (;) at the end of this answer
