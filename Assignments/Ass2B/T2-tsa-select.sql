299fd4ccb68db8a3cfe9c9cd913f0c0fc59c8b93
Garret Yong <gyon0004@student.monash.edu>
2023-05-28 23:04:11 +0800

    Initial push of T2

---
+++ Assignments/Ass2B/T2-tsa-select.sql
@@ -0,0 +1,53 @@
+--*****PLEASE ENTER YOUR DETAILS BELOW*****
+--T2-tsa-select.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Applied Class No:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+/*2(a)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+
+/*2(b)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*2(c)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+
+/*2(d)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+/*2(e)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
+
+
+/*2(f)*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE that your query is formatted and has a semicolon
+-- (;) at the end of this answer
+
