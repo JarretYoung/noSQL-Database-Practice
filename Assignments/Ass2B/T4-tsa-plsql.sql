d8e5aa0461f246436cf4a4d7222ceae7e9f465e3
Garret Yong <gyon0004@student.monash.edu>
2023-05-28 23:04:25 +0800

    Initial push of T4

---
+++ Assignments/Ass2B/T4-tsa-plsql.sql
@@ -0,0 +1,39 @@
+--*****PLEASE ENTER YOUR DETAILS BELOW*****
+--T4-tsa-plsql.sql
+
+--Student ID:
+--Student Name:
+--Unit Code:
+--Applied Class No:
+
+/* Comments for your marker:
+
+*/
+
+SET SERVEROUTPUT ON
+
+--4(a) 
+-- Create a sequence for REVIEW PK
+
+
+-- Complete the procedure below
+CREATE OR REPLACE PROCEDURE prc_insert_review (
+    p_member_id      IN NUMBER,
+    p_poi_id         IN NUMBER,
+    p_review_comment IN VARCHAR2,
+    p_review_rating  IN NUMBER,
+    p_output         OUT VARCHAR2
+) AS
+BEGIN
+    
+END;
+/
+
+-- Write Test Harness for 4(a)
+
+
+--4(b) 
+--Write your trigger statement, 
+--finish it with a slash(/) followed by a blank line
+
+-- Write Test Harness for 4(b)
