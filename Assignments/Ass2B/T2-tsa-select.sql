a9b27d3cc984f42a33c8841c11e95c0c36ebc028
Garret Yong <gyon0004@student.monash.edu>
2023-06-06 13:59:44 +0800

    Did double checking

--- Assignments/Ass2B/T2-tsa-select.sql
+++ Assignments/Ass2B/T2-tsa-select.sql
@@ -21,6 +21,7 @@
 SELECT town_id, town_name, poi_type_id, poi_type_descr, count(poi_type_id) as poi_count 
     FROM tsa.town natural join tsa.point_of_interest natural join tsa.poi_type
     GROUP BY town_id, town_name, poi_type_id, poi_type_descr
+    HAVING  count(poi_type_id) > 1
     ORDER BY town_id, poi_type_descr;
     
 /*2(b)*/
@@ -28,16 +29,28 @@ SELECT town_id, town_name, poi_type_id, poi_type_descr, count(poi_type_id) as po
 -- ENSURE that your query is formatted and has a semicolon
 -- (;) at the end of this answer
  
-SELECT  m1.member_id, 
-        m1.member_gname || ' ' || m1.member_fname as member_name,
-        r1.resort_id,
-        r1.resort_name, 
-        COUNT(m2.member_id_recby) as number_of_recommendations
-    FROM (tsa.member m1 JOIN tsa.resort r1 ON m1.resort_id = r1.resort_id) LEFT JOIN tsa.member m2 ON m1.member_id = m2.member_id_recby
-    GROUP BY m1.member_id, m1.member_gname, m1.member_fname, r1.resort_id, r1.resort_name
-    ORDER BY r1.resort_id, m1.member_id;
-
-
+    
+SELECT  m1.member_id,
+    m1.member_gname || ' ' || m1.member_fname as member_name,
+    r1.resort_id,
+    r1.resort_name,
+    COUNT(m2.member_id_recby) as number_of_recommendations
+FROM
+    (tsa.member m1
+    JOIN tsa.resort r1 ON m1.resort_id = r1.resort_id)
+    LEFT JOIN tsa.member m2 ON m1.member_id = m2.member_id_recby
+GROUP BY
+    m1.member_id, m1.member_gname, m1.member_fname, r1.resort_id, r1.resort_name
+HAVING
+    COUNT(m2.member_id_recby) = (
+        SELECT MAX(COUNT(m2.member_id_recby))
+        FROM (tsa.member m1
+            JOIN tsa.resort r1 ON m1.resort_id = r1.resort_id)
+            LEFT JOIN tsa.member m2 ON m1.member_id = m2.member_id_recby
+        GROUP BY m1.member_id, m1.member_gname, m1.member_fname, r1.resort_id, r1.resort_name
+    )
+ORDER BY
+    r1.resort_id, m1.member_id;   
 /*2(c)*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
 -- ENSURE that your query is formatted and has a semicolon
@@ -47,9 +60,10 @@ SELECT  poi1.poi_id,
         poi1.poi_name,
         NVL(to_char(MAX(r1.review_rating)), 'NR') AS max_rating,
         NVL(to_char(MIN(r1.review_rating)), 'NR') AS min_rating,
-        NVL(to_char(AVG(r1.review_rating)), 'NR') AS avg_rating
+        NVL(to_char(AVG(r1.review_rating), '99.9'), 'NR') AS avg_rating
     FROM tsa.point_of_interest poi1 LEFT JOIN tsa.review r1 ON poi1.poi_id = r1.poi_id
-    GROUP BY poi1.poi_id, poi1.poi_name;    
+    GROUP BY poi1.poi_id, poi1.poi_name    
+    ORDER BY poi1.poi_id;
 
 /*2(d)*/
 -- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
@@ -58,7 +72,7 @@ SELECT  poi1.poi_id,
 SELECT  poi.poi_name,
         pt.poi_type_descr,
         town.town_name,
-        LPAD('Lat: ' || town.town_lat || ' Long: ' || town.town_long,35,' ') as town_location,
+        LPAD('Lat: ' || to_char(town.town_lat,'999.000000') || ' Long: ' || to_char(town.town_long,'999.000000'),35,' ') as town_location,
         NVL(COUNT(r.review_id), 0) AS review_count,
         CASE 
             WHEN to_char(round(COUNT(r.review_id) * 100 / (SELECT COUNT(*) FROM tsa.review), 2),'90.00') = 0 THEN 'No reviews completed'
